711949387020c858019a0d1f2d68f6b0fc52795a
IvanaFD <ivanafigueroa0936@gmail.com>
2026-05-03 16:36:15 -0600

    fix: register routes before app.listen and fix import order

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,12 @@ import authRoutes from './src/routes/auth.routes.js';
 import productoRoutes from './src/routes/producto.routes.js';
 import clienteRoutes from './src/routes/cliente.routes.js';
 import VentaRoutes from './src/routes/venta.routes.js';
+import OrdenRoutes from './src/routes/orden.routes.js';
+import categoriaRoutes from './src/routes/categoria.routes.js';
+import reportRoutes from './src/routes/reporte.routes.js';
+import empleadoRoutes from './src/routes/empleado.routes.js';
+import proveedorRoutes from './src/routes/proveedor.routes.js';
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -20,6 +26,11 @@ app.use('/api/auth', authRoutes);
 app.use('/api/productos', productoRoutes);
 app.use('/api/clientes', clienteRoutes);
 app.use('/api/ventas', VentaRoutes);
+app.use('/api/ordenes', OrdenRoutes);
+app.use('/api/categorias', categoriaRoutes);
+app.use('/api/reportes', reportRoutes);
+app.use('/api/empleados',  empleadoRoutes);
+app.use('/api/proveedores', proveedorRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
